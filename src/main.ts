e8bd0a7d4cb0c50d71dbce75aad183a1aad66906
Benedikt Zoller <benedikt.zoller@multivac.de>
2024-06-20 19:58:38 +0200

    ci: change runner config

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,6 @@ import { createApp } from 'vue';
 import './index.css';
 import App from './App.vue';
 
-import { VueQueryPlugin } from '@tanstack/vue-query';
-
 const app = createApp(App);
 
-app.use(VueQueryPlugin);
 app.mount('#app');
