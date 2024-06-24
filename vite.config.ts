6242a0c55e6b4cf20eff057f6a5fe8cd51652d79
Benedikt Zoller <benedikt.zoller@multivac.de>
2024-06-24 15:25:34 +0200

    ci: fix tsc build

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "jsdom",
-    setupFiles: ['./tests/setup.ts'],
+    setupFiles: ['vitest.setup.ts'],
     coverage: {
       include: ['src/**/*'],
       exclude: ['src/main.ts'],
